bc69b8210867f5e5aa0c77fcda9a60767d87d9d7
Matthew Shiel <mattshiel@gmail.com>
2017-10-18 01:42:06 +0100

    Added <h1> tag to the Guesing Game title

---
+++ 02MakeH1.go
@@ -0,0 +1,29 @@
+// Author: Matthew Shiel
+// Date: 18/10/2017
+// 01GuessingGame
+// Adapted from: https://codegangsta.gitbooks.io/building-web-apps-with-go/content/creating_a_basic_web_app/index.html
+// Info on Go documentation from https://golang.org/doc/articles/wiki/#tmp_3
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// 
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Encapsulate Guessing Game in a <h1> tag
+	title := "<h1>Guessing Game</h1>" 
+
+	// Print <h1> Guessing Game
+	fmt.Fprintln(w, title) 
+}
+
+func main() {
+	// handler handles HTTP requests with the web root ("/") 
+	http.HandleFunc("/", handler)
+
+	// ListenAndServe will start the server and instruct it to listen on port 8080
+    http.ListenAndServe(":8080", nil)
+}
