244313793a9ecf041de5740fb3a16349216c790d
Matthew Shiel <mattshiel@gmail.com>
2017-10-22 22:26:38 +0100

    Added index, fixed issue with FileServer changed to ServeFile

--- 03ServePage.go
+++ 03ServePage.go
@@ -1,21 +1,25 @@
 // Author: Matthew Shiel
-// Date: 18/10/2017
+// Date: 22/10/2017
 // 03ServePage
-// Adapted from http://www.alexedwards.net/blog/serving-static-sites-with-go
+// Info on Go documentation from https://golang.org/doc/articles/wiki/#tmp_3
+// 
 
 package main
 
 import (
-	"log"
 	"net/http"
 )
 
+// 
+func handler(w http.ResponseWriter, r *http.Request) {
+	// EServe the html file instead of hardcoding
+	http.ServeFile(w, r, "index.html") 
+}
+
 func main() {
-	 // Creates the handler to respond to the HTTP request
-	fs := http.FileServer(http.Dir("/02-go-problem-sheet/index.html"))
-	// Handles HTTP requests with the web root ("/") 
-	http.Handle("/", fs)
-  
-	log.Println("Listening...")
-	http.ListenAndServe(":8080", nil)
+	// handler handles HTTP requests with the web root ("/") 
+	http.HandleFunc("/", handler)
+
+	// ListenAndServe will start the server and instruct it to listen on port 8080
+    http.ListenAndServe(":8080", nil)
 }
